c857c16a3d32b54ee23ad22048d66da6764361fc
Rajesh0272 <112319062+Rajesh0272@users.noreply.github.com>
2022-09-21 16:43:24 +0530

    Update bhpp_app.py

--- bhpp_app.py
+++ bhpp_app.py
@@ -25,7 +25,7 @@ def predict():
         bhk=int(request.form['bhk'])
         global  __data_columns
 
-        with open("C:/Users/M RAJESH/PycharmProjects/Flask_vs/artifacts/columns.json", "r") as f:
+        with open("https://raw.githubusercontent.com/Rajesh0272/BHPP/main/artifacts/columns.json", "r") as f:
             __data_columns = json.load(f)['data_columns']
 
         try:
@@ -42,30 +42,6 @@ def predict():
         prediction =model.predict([x])
         output = round(prediction[0]*1000,3)
         
-        return render_template('bhp.html',result=output)
-    if request.method=='POST':
-        location=str(request.form['location'])
-        sqft=float(request.form['sqft'])
-        bath=int(request.form['bath'])
-        bhk=int(request.form['bhk'])
-        global  __data_columns
-
-        with open("C:/Users/M RAJESH/PycharmProjects/Flask_vs/artifacts/columns.json", "r") as f:
-            __data_columns = json.load(f)['data_columns']
-
-        try:
-            loc_index = __data_columns.index(location.lower())
-        except:
-            loc_index = -1
-
-        x = np.zeros(len(__data_columns))
-        x[0] = sqft
-        x[1] = bath
-        x[2] = bhk
-        if loc_index>=0:
-            x[loc_index] = 1
-        prediction =model.predict([x])
-        output = round(prediction[0]*1000,3)
         return render_template('bhp.html',result=output)
     
     #     final_features=get_estimated_price(location,sqft,bath,bhk)
